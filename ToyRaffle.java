fdbd7478768ebd98678d39b558073b1041972608
sdrobyshevsky <sdrobyshevsky@mail.ru>
2023-11-07 23:27:55 +0300

    Added info about task

--- ToyRaffle.java
+++ ToyRaffle.java
@@ -2,12 +2,12 @@ public class ToyRaffle {
     public static void main(String[] args) {
         ToyStore store = new ToyStore();
 
-        Toy toy1 = new Toy(1, "Конструктор Lego", 10, 20);
-        Toy toy2 = new Toy(2, "Трансфомер", 200, 40);
-        Toy toy3 = new Toy(3, "Мяч", 200, 90);
-        Toy toy4 = new Toy(3, "Кукла Barbie", 50, 30);
-        Toy toy5 = new Toy(3, "Пазл", 200, 90);
-        Toy toy6 = new Toy(3, "PlayStation 5", 10, 10);
+        Toy toy1 = new Toy(1, "Конструктор", 10, 20);
+        Toy toy2 = new Toy(2, "Мишка Тедди", 200, 40);
+        Toy toy3 = new Toy(3, "Танк на радиоуправлении", 200, 90);
+        Toy toy4 = new Toy(3, "Футбольный мяч", 50, 30);
+        Toy toy5 = new Toy(3, "Скакалка", 200, 90);
+        Toy toy6 = new Toy(3, "Гоночная машинка на радиоуправлении", 10, 10);
 
         store.addToy(toy1);
         store.addToy(toy2);
