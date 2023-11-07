55162c885e578738a9e00ce8f0c1cccd6af4117c
sdrobyshevsky <sdrobyshevsky@mail.ru>
2023-11-07 23:30:02 +0300

    Added info about task

--- ToyRaffle.java
+++ ToyRaffle.java
@@ -4,9 +4,9 @@ public class ToyRaffle {
 
         Toy toy1 = new Toy(1, "Конструктор", 10, 20);
         Toy toy2 = new Toy(2, "Мишка Тедди", 200, 40);
-        Toy toy3 = new Toy(3, "Танк на радиоуправлении", 200, 90);
+        Toy toy3 = new Toy(3, "Робот", 200, 90);
         Toy toy4 = new Toy(3, "Футбольный мяч", 50, 30);
-        Toy toy5 = new Toy(3, "Скакалка", 200, 90);
+        Toy toy5 = new Toy(3, "Кукла", 200, 90);
         Toy toy6 = new Toy(3, "Гоночная машинка на радиоуправлении", 10, 10);
 
         store.addToy(toy1);
